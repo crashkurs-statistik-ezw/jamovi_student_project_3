e1388027c01fa2669f7371aea4318fc4f48c5d8b
Christian <burkhart.christian@gmail.com>
2023-09-03 18:21:36 +0200

    solution added

---
+++ analysis/analysis.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+
+# Gereinigter Datensatz laden ---------------------------------------------
+data <- read_csv("data/clean/motivation_cleaned.csv") 
+
+
+
+# Deskriptive Statistik ---------------------------------------------------
+jmv::descriptives(
+  formula = final_test ~ practice_type_corrected,
+  data = data,
+  median = FALSE,
+  min = FALSE,
+  max = FALSE)
+
+
+
+# Histogram ---------------------------------------------------------------
+jmv::descriptives(
+  formula = final_test ~ practice_type,
+  data = data,
+  hist = TRUE,
+  n = FALSE,
+  missing = FALSE,
+  mean = FALSE,
+  median = FALSE,
+  sd = FALSE,
+  min = FALSE,
+  max = FALSE)
+
+ggplot(data, aes(x = final_test)) +
+  geom_histogram(binwidth = 5, fill = "gray", color = "black") +
+  labs(title = "Histogramm der final_test Variable (Binwidth = 5)",
+       x = "Final Test Score",
+       y = "Anzahl") +
+  facet_wrap(~ practice_type_corrected) +  # Facettierung nach practice_type
+  theme_minimal()
+
+
+
+# Balkendiagram -----------------------------------------------------------
+jmv::descriptives(
+  data = data,
+  vars = practice_type_corrected,
+  bar = TRUE,
+  n = FALSE,
+  missing = FALSE,
+  mean = FALSE,
+  median = FALSE,
+  sd = FALSE,
+  min = FALSE,
+  max = FALSE)
+
+ggplot(data, aes(x = as.factor(practice_type_corrected))) +
+  geom_bar(fill = "gray", color = "black") +
+  labs(title = "Balkendiagramm für practice_type_corrected",
+       x = "Practice Type Corrected",
+       y = "Häufigkeit") +
+  theme_minimal()
+
+ggplot(data, aes(x = as.factor(practice_type_corrected), y = final_test)) +
+  geom_bar(stat = "summary", fun = "mean", fill = "gray", color = "black") +
+  labs(title = "Balkendiagramm für practice_type_corrected (Mittelwert von final_test)",
+       x = "Practice Type Corrected",
+       y = "Mittelwert von final_test") +
+  theme_minimal()
+
+
+# Boxplot -----------------------------------------------------------------
+jmv::descriptives(
+  formula = final_test ~ practice_type_corrected,
+  data = data,
+  box = TRUE,
+  n = FALSE,
+  missing = FALSE,
+  mean = FALSE,
+  median = FALSE,
+  sd = FALSE,
+  min = FALSE,
+  max = FALSE)
+
+ggplot(data, aes(x = as.factor(practice_type_corrected), y = final_test)) +
+  geom_boxplot(fill = "gray", color = "black") +
+  labs(title = "Boxplot von final_test nach practice_type_corrected",
+       x = "Practice Type Corrected",
+       y = "Final Test Score") +
+  theme_minimal()
--- data/data_quality.R
+++ data/data_quality.R
@@ -12,13 +12,15 @@ view(dframe)
 
 # Data Quality Checks ausführen -------------------------------------------
 dframe |> 
-  verify(has_only_names("id", "practice_type",
+  verify(has_only_names("id", "practice_type", "practice_type_corrected",
                         "interest_foreign_lang", "interest_swedish",
                         "number_languages", "time_free_choice",
                         "retrieval_cycle1", "retrieval_cycle2", 
                         "final_test")) |> 
   assert(within_bounds(1, 5), interest_foreign_lang) |>
   assert(within_bounds(1, 5), interest_swedish) |>
+  assert(in_set("retrieval", "restudy"), practice_type_corrected) |> 
   assert(within_bounds(0, 100), retrieval_cycle1, retrieval_cycle2, final_test) |>
   verify(number_languages > 0) |> 
   verify(time_free_choice >= 0)
+ 
